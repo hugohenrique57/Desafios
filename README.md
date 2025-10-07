0aab7ca46a521e5e1b7ea196cf5fc0400f950042
hugo  henrique da silva ferreira <204108947+hugohenrique57@users.noreply.github.com>
2025-10-07 19:06:33 +0000

    new commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 Desafio de Estrura de dados
+
+
+Objetivos:
+
+    Desenvolver um programa em C que implemente um sistema em linguagem C para cadastro dos dados (nome, cor e tropas) para estruturar as informações básicas do jogo War utilizando um vetor de structs.
+ 
+
+    Implementar a funcionalidade de ataque entre territórios em linguagem C, utilizando alocação dinâmica e ponteiros, para simular batalhas e aproximação do sistema do comportamento estratégico do jogo real.
+ 
+
+    Implementar o sistema de missões estratégicas em linguagem C, utilizando ponteiros e alocação dinâmica, para verificar a condição de vitória e conclusão do desenvolvimento do jogo.
+
+
+Cenário:
+
+    A TechNova, empresa de desenvolvimento de jogos, visionária no mercado de entretenimento digital, decidiu lançar a nova edição do War — agora com muito mais estratégia e controle sobre os recursos computacionais. Contudo, a nova versão exige que a lógica por trás do domínio de territórios seja sólida, escalável e eficiente. Para isso, será necessário implementar o gerenciamento de territórios, ataques, missões e condições de vitória com base em conceitos fundamentais de estrutura de dados e usando a linguagem C.
--- War_Novato.c
+++ War_Novato.c
@@ -1,1 +1,86 @@
+// Desafio novato, tentativa de reproduzir o jogo "War"
 
+//-----------------------------------------------------
+
+//bibliotecas
+#include <stdio.h>
+#include <string.h>
+
+
+
+//estrutura  
+struct territorios
+{
+    char nome[30];
+    char cor[10];
+    int tropas;
+};
+
+int main(){
+    int Escolha;
+    int TotalPaises;
+    
+    printf("---Bem Vindo ao jogo war---\n");
+    printf("Escolha uma das Opcoes abaixo\n");
+    printf("(1) JOGAR\n");
+    printf("(2) Regras!\n");
+    printf("(3) SAIR!...\n");
+    scanf("%d" , &Escolha);
+   
+    switch (Escolha)
+    {
+    case 1:
+        printf("Vamos cadastrar os 5 territorios inicias do nosso mundo.\n");
+        printf("--------------------------------------------------------");
+        for ( int i = 0; i < 5; i++)
+        {
+        struct territorios paises; 
+            printf("Digite o nome:");
+            scanf("%s", &paises.nome);
+
+            printf("\n");
+
+            printf("Digite a Cor:");
+            scanf("%s" , &paises.cor);
+
+            printf("\n");
+
+            printf("Digite o numero de tropas:");
+            scanf("%d", &paises.tropas);
+        
+        }
+        
+        for (int i = 0; i < 5; i++)
+        {
+            printf("paise %d \n", i+1);
+            printf("")
+            printf("")
+            printf("")
+        }
+        
+    
+         
+        break;
+    case 2:
+        printf("...\n");
+        break;
+    case 3: 
+        printf("Saindo...\n");
+    break;
+    default:
+        printf("Opcao Invalida!\n");
+        break;
+    }
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
